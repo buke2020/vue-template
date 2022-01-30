34159b39306b301cfc15c3c22ec75a9e30067771
buke2020 <wpshlln@163.com>
2022-01-30 17:02:51 +0800

    el-select懒加载实现

--- src/views/Demo/VirtualList.vue
+++ src/views/Demo/VirtualList.vue
@@ -1,9 +1,16 @@
 <template>
   <page-wrapper>
-    <template slot="header">虚拟列表</template>
     <template slot="body">
-      <el-select v-model="item" filterable clearable placeholder="输入查询匹配或下拉选择" v-loadmore="sumItemList">
-        <el-option v-for="item in itemList" :key="item.id" :value="item.id" :label="item.name"></el-option>
+      <el-select
+        v-model="lazyValue"
+        filterable
+        clearable
+        placeholder="输入查询匹配或下拉选择"
+        v-loadmore="loadmore(rangeNum)"
+        :filter-method="filterMethod"
+        @visible-change="visibleChange"
+      >
+        <el-option v-for="item in itemList.slice(0, rangeNum)" :key="item.id" :value="item.id" :label="item.name"></el-option>
       </el-select>
     </template>
   </page-wrapper>
@@ -11,4 +18,9 @@
 
 <script lang="ts" src="./VirtualList.ts"></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+/deep/ .el-select {
+  width: 388px;
+  margin: 20px 0;
+}
+</style>
