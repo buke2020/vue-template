945b3da3ed6371b6d77b3dd2c081191141d33ad2
buke2020 <wpshlln@163.com>
2022-02-24 23:53:19 +0800

    feat: ✨ 新增列拖拽事件

--- src/views/Demo/Dragable.ts
+++ src/views/Demo/Dragable.ts
@@ -5,41 +5,64 @@ import Sortable from 'sortablejs'
   components: {}
 })
 export default class Dragable extends Vue {
-  itemList: any[] = [] // 数据项列表
-  sortList: any[] = [] // 排序数组
-  sortTable: any // 排序table
+  rowList: any[] = [] // 行数据集合
+  rowSortList: any[] = [] // 排序后的行数据集合
+  rowTable: any // 行排序table
+
+  columnList: any[] = [] // 列数据集合
+  columnSortList: any[] = [] // 排序后的列数据集合
+  columnTable: any // 列排序table
   $refs: any
 
   created() {
-    for (let i = 1001; i < 1016; i++) {
-      this.itemList.push({ code: i, name: `数据项${i}` })
+    for (let i = 1001; i < 1010; i++) {
+      this.rowList.push({ code: i, name: `数据项${i}` })
+      this.columnList.push({ id: i, code: `代码${i}`, name: `数据项${i}` })
     }
+    this.rowSortList = [...this.rowList]
+    this.columnSortList = [...this.columnList]
   }
 
   mounted() {
-    this.rowDrop() // 开启拖拽
+    this.rowDrop() // 开启行拖拽
+    // this.columnDrop() // 开启列拖拽
   }
 
   beforeDestroy() {
-    this.sortTable.destroy() // 销毁排序table
+    this.rowTable.destroy() // 销毁排序table
   }
 
   /**
-   * 拖拽事件
+   * 行拖拽事件
    */
   rowDrop() {
     const tbody = document.querySelector('.el-table__body-wrapper tbody')
     // eslint-disable-next-line @typescript-eslint/no-this-alias
     const self = this
-    self.sortTable = Sortable.create(tbody, {
+    this.rowTable = Sortable.create(tbody, {
       disabled: false,
       animation: 150, // 拖拽延时
       onEnd(newVal: any, oldVal: any) {
-        const currRow = self.itemList.splice(oldVal, 1)[0]
-        self.itemList.splice(newVal, 0, currRow)
-        const temp: any = self.sortList.splice(newVal.oldIndex, 1)[0]
-        self.sortList.splice(newVal.newIndex, 0, temp)
+        const curRow = self.rowSortList.splice(newVal.oldIndex, 1)[0]
+        self.rowSortList.splice(newVal.newIndex, 0, curRow)
       }
-    }) // 创建排序table
+    }) // 创建行排序table
+  }
+
+  /**
+   * 列拖拽事件
+   */
+  columnDrop() {
+    const tbody = document.querySelector('.el-table__body-wrapper tbody')
+    // eslint-disable-next-line @typescript-eslint/no-this-alias
+    const self = this
+    self.columnTable = Sortable.create(tbody, {
+      animation: 150, // 拖拽延时
+      onEnd(val: any) {
+        console.log(val, 10001)
+        const curColumn = self.columnSortList.splice(val.oldIndex, 1)[0]
+        self.columnSortList.splice(val.newIndex, 0, curColumn)
+      }
+    }) // 创建列排序table
   }
 }
--- src/views/Demo/Dragable.vue
+++ src/views/Demo/Dragable.vue
@@ -1,15 +1,28 @@
 <template>
-  <page-wrapper>
-    <template slot="body">
-      <el-table ref="table" :data="itemList">
-        <el-table-column label="序号">
-          <template slot-scope="scope">{{ scope.$index + 1 }}</template>
-        </el-table-column>
-        <el-table-column prop="name" label="名称" show-overflow-tooltip></el-table-column>
-        <el-table-column prop="code" label="代码" show-overflow-tooltip></el-table-column>
-      </el-table>
-    </template>
-  </page-wrapper>
+  <div>
+    <page-wrapper>
+      <template slot="header">行拖拽</template>
+      <template slot="body">
+        <el-table key="row" :data="rowList">
+          <el-table-column label="序号">
+            <template slot-scope="scope">{{ scope.$index + 1 }}</template>
+          </el-table-column>
+          <el-table-column prop="name" label="名称" show-overflow-tooltip></el-table-column>
+          <el-table-column prop="code" label="代码" show-overflow-tooltip></el-table-column>
+        </el-table>
+      </template>
+    </page-wrapper>
+    <page-wrapper>
+      <template slot="header">列拖拽</template>
+      <template slot="body">
+        <el-table key="column" :data="columnList">
+          <el-table-column prop="id" label="ID" show-overflow-tooltip></el-table-column>
+          <el-table-column prop="name" label="名称" show-overflow-tooltip></el-table-column>
+          <el-table-column prop="code" label="代码" show-overflow-tooltip></el-table-column>
+        </el-table>
+      </template>
+    </page-wrapper>
+  </div>
 </template>
 
 <script lang="ts" src="./Dragable.ts"></script>
